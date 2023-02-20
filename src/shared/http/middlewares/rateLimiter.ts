4154100b2bcd90b27c74a737ff99744e0e5d9ad4
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2023-02-20 20:02:44 -0300

    rate limiter

---
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import Redis from 'ioredis';
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const redisClient = new Redis({
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASS || undefined,
+    });
+
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 1,
+      duration: 1,
+    });
+
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Muitas requisições', 429);
+  }
+}
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,11 +9,13 @@ import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import upload from '@config/upload';
 import { pagination } from 'typeorm-pagination';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
 app.use(pagination);
 app.use('/files', express.static(upload.directory));
 app.use(routes);
